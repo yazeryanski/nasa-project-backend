1142271f24278ea35ceb91467ddf2ed467920bcb
Narek Yazeryan <yazeryann@gmail.com>
2023-02-06 15:46:47 +0400

    code cleaning

--- src/models/launches/launches.model.ts
+++ src/models/launches/launches.model.ts
@@ -2,21 +2,20 @@ import { DMP } from 'models/__helpers/helpers';
 import { ILaunchAddRequest, Launch } from 'types/launches.types';
 import launchesModel from './launches.mongo';
 
-const DEFAULT_FLIGHT_NUMBER = 100;
-
 const getLastFlightNumber = async () => {
+  const DEFAULT_FLIGHT_NUMBER = 100;
   const lastLaunch = await launchesModel.findOne().sort('-flightNumber');
 
   return lastLaunch?.flightNumber || DEFAULT_FLIGHT_NUMBER;
-}
+};
 
 const add = async (launch: ILaunchAddRequest) => {
-  const lastFlightNumber = await getLastFlightNumber()
+  const lastFlightNumber = await getLastFlightNumber();
 
   const launchData = {
     ...launch,
-    flightNumber: lastFlightNumber + 1
-  }
+    flightNumber: lastFlightNumber + 1,
+  };
 
   const newLaunch = new Launch(launchData);
 
@@ -36,14 +35,17 @@ const add = async (launch: ILaunchAddRequest) => {
 const get = () => launchesModel.find({}, DMP).sort('flightNumber');
 
 const abort = (flightNumber: number) => {
-  return launchesModel.updateOne({ flightNumber }, {
-    upcoming: false,
-    success: false
-  })
-}
+  return launchesModel.updateOne(
+    { flightNumber },
+    {
+      upcoming: false,
+      success: false,
+    }
+  );
+};
 
 export default {
   get,
   add,
-  abort
-}
+  abort,
+};
